c2885151456f8abe5962cd4540dead379d8de476
conbert11 <c.berthun@web.de>
2024-06-13 19:17:01 +0200

    fix

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/components/App';
 import { setConfig } from 'react-hot-loader';
-import './Pterodactyl_Nightcore_Theme.css';
+import './xCBTheme.css';
 
 // Enable language support.
 import './i18n';
