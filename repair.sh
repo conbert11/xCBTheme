374f68ed31f5c60ce4b27acf6c0a38fea4978e9e
conbert11 <c.berthun@web.de>
2024-06-14 14:42:43 +0200

    fix

--- repair.sh
+++ repair.sh
@@ -4,29 +4,37 @@ if (( $EUID != 0 )); then
 fi
 
 repairPanel(){
-    cd /var/www/pterodactyl
+    echo -e "${GREEN}Uninstalling xCBTheme...${RESET}"
+cd /var/www/pterodactyl 2>&1
 
-    php artisan down
+    php artisan down 2>&1
 
-    rm -r /var/www/pterodactyl/resources
+    rm -r /var/www/pterodactyl/resources 2>&1
 
     curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
 
-    chmod -R 755 storage/* bootstrap/cache
+    chmod -R 755 storage/* bootstrap/cache 2>&1
 
-    composer install --no-dev --optimize-autoloader
+    composer install --no-dev --optimize-autoloader 2>&1
 
-    php artisan view:clear
+    php artisan view:clear 2>&1
 
-    php artisan config:clear
+    php artisan config:clear 2>&1
 
-    php artisan migrate --seed --force
+    php artisan migrate --seed --force 2>&1
+ 
+    chown -R www-data:www-data /var/www/pterodactyl/* 2>&1
 
-    chown -R www-data:www-data /var/www/pterodactyl/*
+    php artisan queue:restart 2>&1
 
-    php artisan queue:restart
+    php artisan up 2>&1
 
-    php artisan up
+    echo "UNINSTALLING DONE!"
+
+clear
+bash <(curl https://raw.githubusercontent.com/conbert11/xCBTheme/main/install.sh)
+
+
 }
 
 while true; do
